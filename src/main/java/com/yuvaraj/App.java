79b2113177901a62d6b9c825cafe8fcacb90a9e2
yuvarajennam <yuvarajoffl9900@gmail.com>
2026-08-17 11:33:08 +0530

    Update application greeting

--- src/main/java/com/yuvaraj/App.java
+++ src/main/java/com/yuvaraj/App.java
@@ -3,6 +3,6 @@ package com.yuvaraj;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from my first Maven project!");
+        System.out.println("Hello World cmrcet");
     }
 }
