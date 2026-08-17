1fd45f1f0cee3c61e7da587f2d57fab744609c41
yuvarajennam <yuvarajoffl9900@gmail.com>
2026-08-17 10:31:21 +0530

    Initial Maven project

---
+++ src/main/java/com/yuvaraj/App.java
@@ -0,0 +1,8 @@
+package com.yuvaraj;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from my first Maven project!");
+    }
+}
---
+++ src/test/java/com/yuvaraj/AppTest.java
@@ -0,0 +1,12 @@
+package com.yuvaraj;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
